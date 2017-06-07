7a606131309bb3c517bada75c576a850b2ee1610
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2017-06-07 09:02:24 -0700

    Update channel lineup JSON generation

--- src/antennas.cr
+++ src/antennas.cr
@@ -1,25 +1,25 @@
 require "kemal"
 require "json"
 require "yaml"
+require "http/client"
 
 class Config
   YAML.mapping(
-    bindAddress: String,
-    tvheadendURL: String,
-    antennaURL: String,
-    tunerCount: Int32,
-    tvheadendWeight: Int32,
-    chunkSize: String,
+    bind_address: String,
+    tvheadend_url: String,
+    antenna_url: String,
+    tuner_count: Int32,
+    tvheadend_weight: Int32,
+    chunk_size: String,
   )
 end
 
-def parseConfig
-  config = Config.from_yaml(File.read("./config/config.yml"))
+def config
+  Config.from_yaml(File.read("./config/config.yml"))
 end
 
 get "/" do
-  config = parseConfig
-  "Antennas are operational!" + config.antennaURL
+  "Antennas are operational!" + config.antenna_url
 end
 
 get "/discover.json" do |env|
@@ -47,14 +47,34 @@ get "/lineup_status.json" do |env|
   }.to_json
 end
 
+# Scan TVHeadend channels and generate a JSON list
 get "/lineup.json" do |env|
   env.response.content_type = "application/json"
-  # TODO: Scan TVHeadend channels and generate a JSON list
+  response = HTTP::Client.get "http://#{config.tvheadend_url}/api/channel/grid?start=0&limit=999999"
+  channels = JSON.parse response.body
+
+  lineup = [] of String
+  channels["entries"].each do |channel|
+    if channel["enabled"]
+      uuid = channel["uuid"]
+      url = "#{config.tvheadend_url}/stream/channel/#{uuid}/#{config.tvheadend_weight}"
+      lineup << {
+        GuideNumber: channel["number"],
+        GuideName: channel["name"],
+        URL: url
+      }.to_json
+    end
+  end
+
+  lineup.to_json
 end
 
+# What's the point of this guy?
 get "/lineup.post" do |env|
   env.response.content_type = "application/json"
-  # What's the point of this guy?
+  
 end
+
+# TODO: Flight check to make sure tvheadend is up and running and reachable
 
 Kemal.run(5004)
