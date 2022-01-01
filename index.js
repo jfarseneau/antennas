5ab64ff73fbd95bbfa62438484717c68a4cd0aed
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2021-12-31 18:30:07 -0800

    Refactor code to be a bit more functional, and not call config a bunch of times
    Also:
    - Bump the version up to 4.0.0
    - Add a nifty little ASCII logo
    - Add a binary path so it can be run using npx (the main point of this branch)
    - Configs no longer _require_ a config file and can be ENV only
    - Config can now be specified with a custom path

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+#!/usr/bin/env node
+
 const Koa = require('koa');
 const serve = require('koa-static');
 const logger = require('koa-logger');
 const fs = require('fs');
 
-const router = require('./src/router');
-const config = require('./src/config');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+// Load config
+const configHandler = require('./src/configHandler');
+const config = argv.config ? configHandler.loadConfig(argv.config) : configHandler.loadConfig();
+
+// Setup Antenna components
+const device = require('./src/device')(config);
+const router = require('./src/router')(config, device);
 const ssdp = require('./src/ssdp');
 
 // TODO: Figure out the discovery protocol UDP thing on port 65001
@@ -15,8 +26,8 @@ const app = new Koa();
 try {
   app
     .use(logger())
-    .use(router().routes())
-    .use(router().allowedMethods())
+    .use(router.routes())
+    .use(router.allowedMethods())
     .use(serve('public', { extensions: true }))
     .use(async function pageNotFound(ctx) {
       ctx.status = 404;
@@ -25,10 +36,16 @@ try {
     });
 
   app.listen(5004);
-  console.log(`📡  Antennas are deployed! Proxying from ${config().antennas_url}`);
-  ssdp();
+
+  const logo = fs.readFileSync('./assets/logo.txt','utf8');
+  // Version only comes in when run with NPM, so make this optional
+  let antennasVersion = process.env.npm_package_version ? `v${process.env.npm_package_version}` : ''; 
+  console.log('\x1b[34m%s\x1b[0m', logo, antennasVersion);
+  console.log(``)
+  console.log(`📡 Antennas are deployed! Proxying from ${config.antennas_url}`);
+  ssdp.broadcastSSDP(device);
 } catch (e) {
-  console.log('❌  Antennas failed to deploy! 😮  It could be missing a config file, or something is misconfigured. See below for details:');
+  console.log('❌ Antennas failed to deploy! 😮  It could be missing a config file, or something is misconfigured. See below for details:');
   console.log(e);
 }
 
---
+++ src/configHandler.js
@@ -0,0 +1,52 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+function structureConfig(tvheadendUrl, antennasUrl, tunerCount, deviceUuid) {
+  let splitURI = tvheadendUrl.split('@');
+  let parsedTvheadendURI;
+  if (splitURI.length > 1) {
+    let password = splitURI[0].split(':')[2];
+    let username = splitURI[0].split(":")[1].substr(2);
+    let parsedURI = `${splitURI[0].split(":")[0]}://${splitURI[1]}`
+    parsedTvheadendURI = {
+      username: username,
+      password: password,
+      uri: parsedURI,
+    }
+  } else {
+    parsedTvheadendURI = {
+      username: null,
+      password: null,
+      uri: uri,
+    }
+  }
+
+  return {
+    tvheadend_parsed_uri: parsedTvheadendURI.uri,
+    tvheadend_username: parsedTvheadendURI.username,
+    tvheadend_password: parsedTvheadendURI.password,
+    tvheadend_url: tvheadendUrl,
+    antennas_url: antennasUrl,
+    tuner_count: tunerCount,
+    device_uuid: deviceUuid
+  }
+}
+
+function loadConfig(configFile = 'config/config.yml') {
+  // Check if you even need to load the config file
+  if (process.env.TVHEADEND_URL && process.env.ANTENNAS_URL && process.env.TUNER_COUNT && process.env.DEVICE_UUID) {
+    return structureConfig(process.env.TVHEADEND_URL, process.env.ANTENNAS_URL, process.env.TUNER_COUNT, process.env.DEVICE_UUID);
+  }
+
+  // If you do, load it
+  let config = yaml.safeLoad(fs.readFileSync(configFile, 'utf8'));
+  return structureConfig(
+    process.env.TVHEADEND_URL || config.tvheadend_url,
+    process.env.ANTENNAS_URL || config.antennas_url,
+    process.env.TUNER_COUNT || config.tuner_count,
+    process.env.DEVICE_UUID || config.device_uuid,
+  )
+  
+}
+
+module.exports = { loadConfig }
