8b5ad4ecd84deb74c90c83868c5875921de78ed4
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2022-01-16 15:46:36 -0800

    Add test for device

--- src/device.test.js
+++ src/device.test.js
@@ -1,3 +1,26 @@
 const test = require('ava');
+const device = require('./device');
 
-test.todo('Write test');
+test('returns the correct device info', t => {
+  const actual = device({
+    tuner_count: 12,
+    device_uuid: '000-000-000',
+    antennas_url: 'https://antennas.test'
+  })
+
+  expected = {
+    FriendlyName: 'HDHomerun (Antennas)',
+    Manufacturer: 'Silicondust',
+    ManufacturerURL: 'https://github.com/jfarseneau/antennas',
+    ModelNumber: 'HDTC-2US',
+    FirmwareName: 'hdhomeruntc_atsc',
+    TunerCount: 12,
+    FirmwareVersion: '20170930',
+    DeviceID: '000-000-000',
+    DeviceAuth: 'test1234',
+    BaseURL: 'https://antennas.test',
+    LineupURL: 'https://antennas.test/lineup.json'
+  }
+
+  t.deepEqual(actual, expected);
+});
