549057442e78f0b5db83c52c1dc40dbd83714946
Jean-Francois Arseneau <jf.arseneau@gmail.com>
2022-01-16 18:15:34 -0800

    Fix lineup test

--- src/lineup.test.js
+++ src/lineup.test.js
@@ -6,16 +6,6 @@ const lineup = require('./lineup');
 const tvheadendApi = require('./tvheadendApi');
 
 test('calls the API with the right options', async t => {
-  const config = { 
-    url: 'https://test.test',
-    method: 'GET',
-    responseType: 'json',
-    auth: {
-      username: 'user',
-      password: 'pass'
-    }
-  }
-  sinon.stub(tvheadendApi, "get").returns(config);
   const expectedResponse = {
     entries: [{
       enabled: true,
@@ -24,6 +14,8 @@ test('calls the API with the right options', async t => {
       uuid: 'test-uuid'
     }]
   }
+  sinon.stub(tvheadendApi, "get").resolves(expectedResponse);
+  
   sinon.stub(axios, "get").resolves(expectedResponse);  
   const actual = await lineup({tvheadend_stream_url: 'https://stream.test'});
 
